3d4aa30c74c5dd5dac7ba2283b6c3f2530daaea5
rukav <82444499+rukavyshnykov@users.noreply.github.com>
2023-07-04 14:56:27 +0300

    translations

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,15 +3,25 @@ import { useTheme } from 'app/providers/ThemeProvider'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { AppRouter } from './providers/router'
 import { Navbar } from 'widgets/Navbar'
+import { Sidebar } from 'widgets/Sidebar'
+import { Suspense } from 'react'
+import { useTranslation } from 'react-i18next'
+import 'shared/config/i18n/i18n'
+
 
 
 export const App = () => {
     const { theme } = useTheme()
 
     return (
-        <div className={classNames('app', {hovered: true, selected: true}, [theme, 'red', 'blue'])}>
-            <Navbar className=''/>
-            <AppRouter />
+        <div className={classNames('app', {}, [theme, 'red', 'blue'])}>
+            <Suspense fallback=''>
+                <Navbar className='' />
+                <div className='content-page'>
+                    <Sidebar className='' />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     )
 }
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './Navbar.module.scss'
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
-import { ThemeSwitcher } from 'widgets/ThemeSwitcher'
 
 interface NavbarProps {
     className: string
@@ -11,7 +10,6 @@ export const Navbar = ({ className }: NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [])}>
             <div className={cls.links}>
-                <ThemeSwitcher className='' />
                 <AppLink theme={AppLinkTheme.SECONDARY} className={cls.mainlink} to={'/'}>Главная</AppLink>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>О сайте</AppLink>
             </div>
